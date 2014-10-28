eb1bc05ff3f9e3bf8eeb2c2d48ce45208b650031
Baptiste Fontaine <batifon@yahoo.fr>
2014-10-28 21:02:58 +0100

    dtd2xsd added

---
+++ Formula/dtd2xsd.rb
@@ -0,0 +1,19 @@
+require "formula"
+
+class Dtd2xsd < Formula
+  homepage "http://www.w3.org/2000/04/schema_hack/"
+  url "http://lists.xml.org/archives/xml-dev/200101/bin00000.bin", :using => :nounzip
+  sha1 "6631137937e5e6549f387847159bea89bc88c021"
+  version "2001-01-10"
+
+  def install
+    # a patch would fail since it doesn't handle trailing \r's
+    inreplace 'bin00000.bin', "perl\r", '/usr/bin/perl'
+    bin.install 'bin00000.bin' => 'dtd2xsd'
+  end
+
+  test do
+    (testpath/"test.dtd").write "<!ELEMENT foo EMPTY #REQUIRED>"
+    system "#{bin}/dtd2xsd", testpath/"test.dtd"
+  end
+end
