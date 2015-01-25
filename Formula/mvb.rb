353b049ef54dc80fb466655c954c14791d154e8e
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-25 22:32:04 +0100

    mvb: modernize, test improved

--- Formula/mvb.rb
+++ Formula/mvb.rb
@@ -1,18 +1,15 @@
-require "formula"
-
 class Mvb < Formula
-  homepage 'https://github.com/bfontaine/mvb#readme'
+  homepage "https://github.com/bfontaine/mvb#readme"
   url "https://github.com/bfontaine/mvb/archive/0.1.0.tar.gz"
   sha1 "dab1579349141a5df08749a64f7011299d1a08ad"
 
   def install
-    bin.install 'mvb'
+    bin.install "mvb"
   end
 
   test do
-    system 'touch', 'foo'
-    system 'mv', 'foo', 'bar'
-    system "#{bin}/mvb", 'foo', 'bar'
-    system '[ -f foo ]'
+    touch "bar"
+    system "#{bin}/mvb", "foo", "bar"
+    assert File.exist? "foo"
   end
 end
