7441f379ccd648caf8b5a103f782a1e349d88ba8
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-25 22:33:49 +0100

    ncurses-2048: modernize

--- Formula/ncurses-2048.rb
+++ Formula/ncurses-2048.rb
@@ -1,9 +1,6 @@
-require "formula"
-
 # "2048" is not a valid class name
 class Ncurses2048 < Formula
   homepage "https://github.com/alewmoose/2048-in-terminal"
-
   head "https://github.com/alewmoose/2048-in-terminal.git"
 
   def install
@@ -12,17 +9,14 @@ class Ncurses2048 < Formula
   end
 
   test do
-    # FIXME override HOME (setting ENV doesn’t work, nor supplying a hash as
+    # FIXME: override HOME (setting ENV doesn't work, nor supplying a hash as
     # the first arg of popen3)
     home = File.expand_path "~"
     save = "#{home}/.2048"
     backup = "#{testpath}/_2048"
-    mv save, backup if File.exists? save
-    require "open3"
-    Open3.popen3("#{bin}/2048") do |stdin, stdout, _|
-      stdin.write("q")
-    end
+    mv save, backup if File.exist? save
+    pipe_output("#{bin}/2048", "q")
     rm_f save
-    mv backup, save if File.exists? backup
+    mv backup, save if File.exist? backup
   end
 end
