dbddf26df4d49a8c251a2460d91d1df5512c010c
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-25 22:28:10 +0100

    homer: modernize

--- Formula/homer.rb
+++ Formula/homer.rb
@@ -1,18 +1,15 @@
-require "formula"
-
 class Homer < Formula
   homepage "https://github.com/bfontaine/homer"
   url "https://github.com/bfontaine/homer/archive/0.1.3.tar.gz"
   sha1 "74f61ec863d1305e4e808fa97000d74a14bd3843"
 
   def install
-    ENV['PREFIX'] = prefix
-    system "make", "install"
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
-    system "#{bin}/homer -O mylog touch a_file"
-    system "[ -f mylog ]"
-    system "[ -f a_file ]"
+    system "#{bin}/homer", "-O", "mylog", "touch", "a_file"
+    assert File.exist? "mylog"
+    assert File.exist? "a_file"
   end
 end
