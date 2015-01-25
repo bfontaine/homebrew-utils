14a35ab6b8dbbf315fb4baad20ab83025d5cfb6c
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-25 22:32:58 +0100

    up: modernize

--- Formula/up.rb
+++ Formula/up.rb
@@ -1,18 +1,16 @@
-require "formula"
-
 class Up < Formula
-  homepage 'https://github.com/bfontaine/up#readme'
+  homepage "https://github.com/bfontaine/up#readme"
   url "https://github.com/bfontaine/up/archive/0.1.5.tar.gz"
   sha1 "d7d1087d051c0fd61cafecaf583f7900fbdf3929"
 
   def install
-    bin.install 'bin/up'
+    bin.install "bin/up"
     bash_completion.install "utils/up.bash"
   end
 
   test do
-    ENV['COMMANDS'] = pwd+'/foo'
-    touch 'foo'
+    ENV["COMMANDS"] = pwd+"/foo"
+    touch "foo"
     system "#{bin}/up", "--add", "x", "touch y"
     system "#{bin}/up", "x"
     system "#{bin}/up", "--rm", "x"
