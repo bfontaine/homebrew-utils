39c5be3023ff2bf6215ccf2f48a56bdc8c4aa48e
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-15 23:24:40 +0100

    iwc: head added, simpler test

--- Formula/iwc.rb
+++ Formula/iwc.rb
@@ -2,17 +2,13 @@ class Iwc < Formula
   homepage "https://github.com/bfontaine/iwc"
   url "https://github.com/bfontaine/iwc/archive/0.0.1.tar.gz"
   sha1 "09b4e249d55cf2bd22dc0fb8d9033bd8eb994df8"
+  head "https://github.com/bfontaine/iwc.git"
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
-    (testpath/"test").write <<-EOS.undent
-      foo
-      bar
-    EOS
-
-    assert_match "2\n", pipe_output("#{bin}/iwc", File.read(testpath/"test"))
+    assert_match "2\n", pipe_output("#{bin}/iwc", "foo\nbar\n")
   end
 end
