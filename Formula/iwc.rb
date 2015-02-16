fd1c6e352b9a169c4a6c727f0edba6a0dfcc5e39
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-16 10:07:21 +0100

    iwc 0.1.0

--- Formula/iwc.rb
+++ Formula/iwc.rb
@@ -1,7 +1,7 @@
 class Iwc < Formula
   homepage "https://github.com/bfontaine/iwc"
-  url "https://github.com/bfontaine/iwc/archive/0.0.1.tar.gz"
-  sha1 "09b4e249d55cf2bd22dc0fb8d9033bd8eb994df8"
+  url "https://github.com/bfontaine/iwc/archive/0.1.0.tar.gz"
+  sha1 "847d2525494d0d4d40ff77ba0f1aff6d4ef85ae0"
   head "https://github.com/bfontaine/iwc.git"
 
   def install
