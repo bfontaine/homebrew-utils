dd3b822273dba9d1d64d9a6fd6281026236d7255
Baptiste Fontaine <b@ptistefontaine.fr>
2016-06-17 22:22:40 +0200

    mediaextract 1.0 (new formula)

---
+++ Formula/mediaextract.rb
@@ -0,0 +1,16 @@
+class Mediaextract < Formula
+  desc "Extracts media files embedded within other files"
+  homepage "https://panzi.github.io/mediaextract/"
+  url "https://github.com/panzi/mediaextract/archive/v1.0.tar.gz"
+  sha256 "56b0198ddd8f24e1d1b664120928133b495e407166022e8f39de7e917bc78787"
+
+  def install
+    system "make", "builddir"
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "#{bin}/mediaextract", "-s", "#{bin}/mediaextract"
+  end
+end
