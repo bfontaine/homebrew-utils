ca860ea5ec87563e9ed6c950239ff3158cdb1314
Baptiste Fontaine <b@ptistefontaine.fr>
2017-05-22 00:17:31 +0200

    rfc 0.2.3

--- Formula/rfc.rb
+++ Formula/rfc.rb
@@ -1,7 +1,7 @@
 class Rfc < Formula
   homepage "https://github.com/bfontaine/rfc#readme"
-  url "https://github.com/bfontaine/rfc/archive/v0.2.2.tar.gz"
-  sha256 "fd6f3a820a0c16eb074110cf46c4df9c3b76ae6b4c6ed68dca1a59e5fd75dfaa"
+  url "https://github.com/bfontaine/rfc/archive/v0.2.3.tar.gz"
+  sha256 "03b51b1045c61d6b2ebd563d47f3f3c36c7a4f37d5c4b7d29b0c8aa073e8e2d3"
 
   def install
     bin.install "rfc"
