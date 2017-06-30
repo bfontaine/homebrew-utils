f012fe638265b0790f549528c2140fac677e384f
Baptiste Fontaine <b@ptistefontaine.fr>
2017-06-30 16:51:38 +0200

    node-compiler 1.1.0 (new formula)

---
+++ Formula/node-compiler.rb
@@ -0,0 +1,20 @@
+class NodeCompiler < Formula
+  desc "Ahead-of-time (AOT) Compiler designed for Node.js, that just works."
+  homepage "http://enclose.io"
+  url "https://github.com/pmq20/node-compiler/archive/v1.1.0.tar.gz"
+  sha256 "9c06048b206246c4dfe08bde875df1f5c87391a2b306389c0bc61640e1649706"
+
+  depends_on "node"
+  depends_on "squashfs"
+
+  def install
+    libexec.install %w[lib node bin]
+
+    bin.write_exec_script libexec/"bin/nodec"
+  end
+
+  test do
+    # Even a simple test would take minutes to complete
+    system bin/"nodec", "--examples"
+  end
+end
