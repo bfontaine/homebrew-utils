895b8d4ff0afcdcf57985a9cf056524354ceb86e
Baptiste Fontaine <b@ptistefontaine.fr>
2017-07-26 15:42:29 +0200

    eq 0.2.2

--- Formula/eq.rb
+++ Formula/eq.rb
@@ -1,9 +1,9 @@
 class Eq < Formula
   desc "EDN equivalent of jq."
   homepage "https://github.com/oscaro/eq"
-  url "https://github.com/oscaro/eq/releases/download/0.2.1/eq.js",
+  url "https://github.com/oscaro/eq/releases/download/0.2.2/eq.js",
     :nounzip => true
-  sha256 "c9b93333cd3ceac654c53c61032556c23a33f39b7a865f62c9c2355e8564dd17"
+  sha256 "ce91294f513cca5c714e372ee9623d5dde11a29fd0bd6f73290fcf6e9956078c"
 
   depends_on "node" => :run
 
