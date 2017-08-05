b68ff44b7bdb52ed15bc8a978b9b255b2adc92ee
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-05 12:18:59 +0200

    iwc: Add desc

--- Formula/iwc.rb
+++ Formula/iwc.rb
@@ -1,4 +1,5 @@
 class Iwc < Formula
+  desc "Interactive wc(1)"
   homepage "https://github.com/bfontaine/iwc"
   url "https://github.com/bfontaine/iwc/archive/0.1.3.tar.gz"
   sha256 "2b17506c44caec574682bec4f56ec08d3cdd3c36f00f9489bdb192ad378ca81e"
