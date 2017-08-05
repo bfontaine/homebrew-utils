db6bb72b74bb44435ac38206a6e21b3dbb65533e
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-05 12:21:54 +0200

    mvb: Add desc

--- Formula/mvb.rb
+++ Formula/mvb.rb
@@ -1,4 +1,5 @@
 class Mvb < Formula
+  desc "Utility to move files back"
   homepage "https://github.com/bfontaine/mvb#readme"
   url "https://github.com/bfontaine/mvb/archive/0.1.0.tar.gz"
   sha256 "cbc3e6de1f1bb422af472df7ecc9c918041a6bc5bd9fa3f0adc87229619e89e0"
