7859effc1e3f4ab4233d875d2cb0b5aeca1edf5c
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-05 12:21:09 +0200

    rfc: Add desc

--- Formula/rfc.rb
+++ Formula/rfc.rb
@@ -1,4 +1,5 @@
 class Rfc < Formula
+  desc "Bash tool to read RFCs from the command-line"
   homepage "https://github.com/bfontaine/rfc#readme"
   url "https://github.com/bfontaine/rfc/archive/v0.2.3.tar.gz"
   sha256 "03b51b1045c61d6b2ebd563d47f3f3c36c7a4f37d5c4b7d29b0c8aa073e8e2d3"
