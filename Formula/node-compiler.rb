43d312ff8bd9c353a48ebe9c91414716439a6395
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-05 12:24:44 +0200

    node-compiler: Fix desc length

--- Formula/node-compiler.rb
+++ Formula/node-compiler.rb
@@ -1,5 +1,5 @@
 class NodeCompiler < Formula
-  desc "Ahead-of-time (AOT) Compiler designed for Node.js, that just works."
+  desc "Ahead-of-time (AOT) Compiler designed for Node.js."
   homepage "http://enclose.io"
   url "https://github.com/pmq20/node-compiler/archive/v1.1.0.tar.gz"
   sha256 "9c06048b206246c4dfe08bde875df1f5c87391a2b306389c0bc61640e1649706"
