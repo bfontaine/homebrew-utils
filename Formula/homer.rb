bd1dd5eca716b6d8ed9a4b17872bd71bec49bfeb
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-05 12:18:28 +0200

    homer: Add desc

--- Formula/homer.rb
+++ Formula/homer.rb
@@ -1,4 +1,5 @@
 class Homer < Formula
+  desc "Command-line tool to daemonize a task."
   homepage "https://github.com/bfontaine/homer"
   url "https://github.com/bfontaine/homer/archive/0.1.3.tar.gz"
   sha256 "a76936289178ce4d8806aebba1add986d49c2f86c04c78f682134d9deb1d3de4"
