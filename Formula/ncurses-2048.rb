8c5655274402ce9c7161374c06b530067defbc82
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-05 12:29:57 +0200

    ncurses-2048: Add desc

--- Formula/ncurses-2048.rb
+++ Formula/ncurses-2048.rb
@@ -1,11 +1,16 @@
 # "2048" is not a valid class name
 class Ncurses2048 < Formula
+  desc "Animated console version of 2048 game"
   homepage "https://github.com/alewmoose/2048-in-terminal"
   head "https://github.com/alewmoose/2048-in-terminal.git"
 
+  conflicts_with "c2048",
+    :because => "both install a '2048' executable"
+
   def install
     system "make"
-    bin.install "2048"
+    bin.mkpath
+    system "make", "install", "DESTDIR=#{bin}"
   end
 
   test do
