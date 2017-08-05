2fa60fd700f19b7fbb8b959f0be4b49c6f8fa238
Baptiste Fontaine <b@ptistefontaine.fr>
2017-08-05 12:15:42 +0200

    smuggler 0.5.2

--- Formula/smuggler.rb
+++ Formula/smuggler.rb
@@ -1,24 +1,28 @@
-require "language/go"
-
 class Smuggler < Formula
+  desc "Tool to run and build containers with Docker"
   homepage "https://github.com/jbdalido/smg"
   url "https://github.com/jbdalido/smg.git",
-    :revision => "d27f29b4290cf73b2ff1b2315bf611131189f7b0"
-  version "0.5.1"
+    :revision => "9bf6185f53fb4d75a004ee27aa5db68def52345e"
+  version "0.5.2"
 
   head "https://github.com/jbdalido/smg.git"
 
   depends_on "go"
 
   def install
+    # Fix an import path. Should be fixed in the next version
+    %w[archive archive_unix changes diff wrap].each do |file|
+      inreplace "Godeps/_workspace/src/github.com/docker/docker/pkg/archive/#{file}.go",
+        "github.com/jbdalido/smg/Godeps/_workspace/src/github.com/docker/docker/vendor/src/",
+        "src/"
+    end
+
     mkdir_p "#{buildpath}/src/github.com/jbdalido/"
     ln_s buildpath, "#{buildpath}/src/github.com/jbdalido/smg"
 
     ENV["GOPATH"] = buildpath
-    ENV.append_path "PATH", "#{ENV["GOPATH"]}/bin"
 
-    system "go", "build", "-o", "smg"
-    bin.install "smg"
+    system "go", "build", "-o", "#{bin}/smg"
   end
 
   test do
