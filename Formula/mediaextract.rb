740efcefc2aa0692d998d7c2c84d13d33b56fa84
Baptiste Fontaine <b@ptistefontaine.fr>
2017-12-24 01:59:54 +0100

    mediaextract: Add HEAD

--- Formula/mediaextract.rb
+++ Formula/mediaextract.rb
@@ -3,6 +3,7 @@ class Mediaextract < Formula
   homepage "https://panzi.github.io/mediaextract/"
   url "https://github.com/panzi/mediaextract/archive/v1.0.tar.gz"
   sha256 "56b0198ddd8f24e1d1b664120928133b495e407166022e8f39de7e917bc78787"
+  head "https://github.com/panzi/mediaextract.git"
 
   def install
     system "make", "builddir"
