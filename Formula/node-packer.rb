00006d52285dba1bacf9a68e5424664563aacf8b
Baptiste Fontaine <b@ptistefontaine.fr>
2017-12-24 01:57:40 +0100

    node-packer 1.5.0

--- Formula/node-packer.rb
+++ Formula/node-packer.rb
@@ -1,20 +1,19 @@
-class NodeCompiler < Formula
+class NodePacker < Formula
   desc "Ahead-of-time (AOT) Compiler designed for Node.js."
   homepage "http://enclose.io"
-  url "https://github.com/pmq20/node-compiler/archive/v1.1.0.tar.gz"
-  sha256 "9c06048b206246c4dfe08bde875df1f5c87391a2b306389c0bc61640e1649706"
+  url "https://github.com/pmq20/node-packer/archive/v1.5.0.tar.gz"
+  sha256 "0bb94148ca130c2bd252caac46ab56cf32a82165746c16cd68411a75b669b378"
 
   depends_on "node"
   depends_on "squashfs"
 
   def install
     libexec.install %w[lib node bin]
-
     bin.write_exec_script libexec/"bin/nodec"
   end
 
   test do
     # Even a simple test would take minutes to complete
-    system bin/"nodec", "--examples"
+    system bin/"nodec", "--version"
   end
 end
