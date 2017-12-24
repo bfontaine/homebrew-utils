071da963b78d2891c2059635d5b26bfb717755b4
Baptiste Fontaine <b@ptistefontaine.fr>
2017-12-24 01:49:10 +0100

    puck 2.3.58

--- Formula/puck.rb
+++ Formula/puck.rb
@@ -1,15 +1,14 @@
 class Puck < Formula
   desc "Program for the analysis of kinship network morphologies"
   homepage "https://www.kinsources.net/editorial/tools.xhtml"
-  url "http://kintip.net/index.php?option=com_jdownloads&task=download.send&id=217&catid=3&m=0",
-    :using => :nounzip
-  version "2.2.13"
-  sha256 "56fe2cf212171e0bd6dd5c3ce71d3a62c03926db7e22b0102f3ff2fec5e90612"
+  url "http://kintip.net/index.php?option=com_jdownloads&task=download.send&id=255&catid=3&m=0"
+  version "2.3.58"
+  sha256 "6440d4a243af3e79c29b3612f9dd859e3932306ad37e24bee5ca7fb1578cdaab"
 
   depends_on :java => "1.6+"
 
   def install
-    libexec.install "index.php" => "puck.jar"
+    libexec.install "puck-#{version}/puck.jar"
     bin.write_jar_script libexec/"puck.jar", "puck"
   end
 
