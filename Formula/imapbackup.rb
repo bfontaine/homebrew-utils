08114bdd7528f62496558d16342ae720fa549f6e
Baptiste Fontaine <b@ptistefontaine.fr>
2018-02-10 00:53:31 +0100

    imapbackup: fix url audit

--- Formula/imapbackup.rb
+++ Formula/imapbackup.rb
@@ -1,10 +1,10 @@
 class Imapbackup < Formula
   desc "IMAP Backup Tool"
   homepage "http://tamale.net/imapbackup/"
-  # unfortunately no tag for now
-  url "https://github.com/rcarmo/imapbackup/archive/5762b005e6fc87d78593052dcfbf08a5853dfe4a.zip"
+  # no tag, unfortunately
+  url "https://github.com/rcarmo/imapbackup/archive/5762b005e6fc87d78593052dcfbf08a5853dfe4a.tar.gz"
   version "1.4g"
-  sha256 "5f4cccf2a9076fefe496dca6409ca2f85b5a7bf59faaad4be930809a1625231d"
+  sha256 "b543a08e14050df7d7261f143e6740bb1a978e4c3dd56608cabc50ec167cf492"
 
   depends_on "python" if MacOS.version <= :snow_leopard
 
