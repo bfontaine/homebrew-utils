fba5492112166b1df9ad17dd6b48763b55923aa2
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2018-05-14 14:36:37 +0200

    git-vanity-sha HEAD (new formula)

---
+++ Formula/git-vanity-sha.rb
@@ -0,0 +1,17 @@
+class GitVanitySha < Formula
+  desc "vanity hex prefixes for your commit SHAs"
+  homepage "https://github.com/mattbaker/git-vanity-sha"
+  head "https://github.com/mattbaker/git-vanity-sha.git"
+
+  def install
+    bin.install "git-vanity-sha"
+  end
+
+  test do
+    system "git", "init"
+    system "git", "config", "user.email", "git-vanity-sha@test.example.com"
+    system "git", "commit", "--allow-empty", "-m", "test"
+    assert_match "SHA found:",
+      pipe_output("#{bin}/git-vanity-sha a", "y\n")
+  end
+end
