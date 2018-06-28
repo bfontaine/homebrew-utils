4ef4c62a66e9bb9b8a0d181693fa26eac05b924e
Baptiste Fontaine <b@ptistefontaine.fr>
2018-06-28 11:19:49 +0200

    rfc 0.2.5

--- Formula/rfc.rb
+++ Formula/rfc.rb
@@ -1,8 +1,8 @@
 class Rfc < Formula
   desc "Bash tool to read RFCs from the command-line"
   homepage "https://github.com/bfontaine/rfc#readme"
-  url "https://github.com/bfontaine/rfc/archive/v0.2.4.tar.gz"
-  sha256 "b707e3ea15b9478b802b2e1d140ce45b2fa537334134b52a3dfad5e809150464"
+  url "https://github.com/bfontaine/rfc/archive/v0.2.5.tar.gz"
+  sha256 "6a1355bf40485a5dcbb4a673b8d23a22cbae132c0e0a6cf82f8c3d3ecf055ff1"
 
   def install
     bin.install "rfc"
