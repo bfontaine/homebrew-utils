4a5ce1f1d858eb87a74bbe238f74fa31641c53a2
Baptiste Fontaine <b@ptistefontaine.fr>
2018-10-28 12:57:22 +0100

    rfc: Add HEAD

--- Formula/rfc.rb
+++ Formula/rfc.rb
@@ -4,8 +4,12 @@ class Rfc < Formula
   url "https://github.com/bfontaine/rfc/archive/v0.2.5.tar.gz"
   sha256 "6a1355bf40485a5dcbb4a673b8d23a22cbae132c0e0a6cf82f8c3d3ecf055ff1"
 
+  head "https://github.com/bfontaine/rfc.git"
+
   def install
     bin.install "rfc"
+    # Remove me when updating to >0.2.5
+    man1.install "man/rfc.1" if build.head?
   end
 
   test do
