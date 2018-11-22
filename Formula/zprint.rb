66cefde84da886c2eff571a32b384c8d1e64e854
Baptiste Fontaine <b@ptistefontaine.fr>
2018-11-22 15:24:21 +0100

    zprint 0.4.13

--- Formula/zprint.rb
+++ Formula/zprint.rb
@@ -1,32 +1,18 @@
 class Zprint < Formula
-  desc "Library to reformat Clojure and Clojurescript source code and s-expressions"
+  desc "Library to reformat Clojure(Script) source code and s-expressions"
   homepage "https://github.com/kkinnear/zprint"
-  url "https://github.com/kkinnear/zprint/releases/download/0.4.7/zprint-filter-0.4.7",
-    :using => :nounzip
-  sha256 "22c8f9651842bba96c27b4a8202ed5d938bc24c5c288899400d622f6569bb5a0"
-
-  depends_on "jdk@8"
-
-  resource "appcds" do
-    url "https://github.com/kkinnear/zprint/releases/download/0.4.7/appcds",
+  if OS.linux?
+    url "https://github.com/kkinnear/zprint/releases/download/0.4.13/zprintl-0.4.13",
       :using => :nounzip
-    sha256 "97ca0722abe9369817d87054294d5a280724a68adf9f299035b33fcebe7f1af6"
+    sha256 "6477333c9e3abb978632cd3212840fdc769e2233a4743ea2adddbb17c86d11ef"
+  else
+    url "https://github.com/kkinnear/zprint/releases/download/0.4.13/zprintm-0.4.13",
+      :using => :nounzip
   end
 
   def install
-    jar = "zprint-filter-#{version}.jar"
-    libexec.install "zprint-filter-#{version}" => jar
-    resource("appcds").stage libexec
-
-    bin.mkpath
-
-    libexec.cd do
-      system "/bin/bash", "appcds", jar, "#{bin}/zprint"
-    end
-
-    inreplace bin/"zprint" do |s|
-      s.gsub!(/^java/, (Formula["jdk@8"].opt_bin/"java").to_s)
-    end
+    suffix = OS.linux? ? "l" : "m"
+    bin.install "zprint#{suffix}-#{version}" => "zprint"
   end
 
   test do
