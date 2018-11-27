23517adb65601f8246cad89f43e46ab43afd11d8
Baptiste Fontaine <b@ptistefontaine.fr>
2018-11-28 00:15:30 +0100

    chromedriver 2.44 (new formula)

---
+++ Formula/chromedriver.rb
@@ -0,0 +1,15 @@
+class Chromedriver < Formula
+  desc "Web driver for Chrome"
+  homepage "https://sites.google.com/a/chromium.org/chromedriver/home"
+  url "https://chromedriver.storage.googleapis.com/2.44/chromedriver_mac64.zip"
+  version "2.44"
+  sha256 "3fd49c2782a5f93cb48ff2dee021004d9a7fb393798e4c4807b391cedcd30ed9"
+
+  def install
+    bin.install "chromedriver"
+  end
+
+  test do
+    system bin/"chromedriver", "--version"
+  end
+end
