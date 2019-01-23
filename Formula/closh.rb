15ae15f80334abb693492791f280acb37670348c
Baptiste Fontaine <b@ptistefontaine.fr>
2019-01-23 14:03:37 +0100

    closh 0.3.3 (new formula)

---
+++ Formula/closh.rb
@@ -0,0 +1,17 @@
+class Closh < Formula
+  desc "Bash-like shell based on Clojure"
+  homepage "https://github.com/dundalek/closh"
+  url "https://github.com/dundalek/closh/releases/download/v0.3.3/closh-zero.jar",
+    :using => :nounzip
+  sha256 "475664d6e2f5aa5ab63263350ef9d4c4aedd738de27ef1da865abdefc3b6f70c"
+  version "0.3.3"
+
+  def install
+    bin.install "closh-zero.jar" => "closh"
+  end
+
+  test do
+    # We can’t write closh scripts for now
+    assert File.executable? bin/"closh"
+  end
+end
