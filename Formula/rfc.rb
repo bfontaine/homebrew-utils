2c74f27ec07bd0a4563e0d51c322424be04faac4
Baptiste Fontaine <b@ptistefontaine.fr>
2019-05-11 12:54:08 +0200

    rfc 0.2.6

--- Formula/rfc.rb
+++ Formula/rfc.rb
@@ -1,15 +1,14 @@
 class Rfc < Formula
   desc "Bash tool to read RFCs from the command-line"
   homepage "https://github.com/bfontaine/rfc#readme"
-  url "https://github.com/bfontaine/rfc/archive/v0.2.5.tar.gz"
-  sha256 "6a1355bf40485a5dcbb4a673b8d23a22cbae132c0e0a6cf82f8c3d3ecf055ff1"
+  url "https://github.com/bfontaine/rfc/archive/v0.2.6.tar.gz"
+  sha256 "724c47827ff1009359919a6fbdc9bb73d35c553546173f65058febca722f9931"
 
   head "https://github.com/bfontaine/rfc.git"
 
   def install
     bin.install "rfc"
-    # Remove me when updating to >0.2.5
-    man1.install "man/rfc.1" if build.head?
+    man1.install "man/rfc.1"
   end
 
   test do
