0a5862a386fc89505e419f3910e4144d0a105e37
Baptiste Fontaine <b@ptistefontaine.fr>
2019-09-16 21:51:45 +0200

    gzsize 0.1.4

--- Formula/gzsize.rb
+++ Formula/gzsize.rb
@@ -1,8 +1,8 @@
 class Gzsize < Formula
   desc "Print the uncompressed size of a GZip file"
   homepage "https://bfontaine.github.io/gzsize/"
-  url "https://github.com/bfontaine/gzsize/archive/0.1.3.tar.gz"
-  sha256 "1d021710b3ff43bfe3ee115beb477953f756a4f967be3f03a7c5f8023f10a77d"
+  url "https://github.com/bfontaine/gzsize/archive/0.1.4.tar.gz"
+  sha256 "0c2331e2a92131ba4dde4810a91fcc090771e18a2871b06df0d7b21d554d38d6"
 
   head "https://github.com/bfontaine/gzsize.git"
 
