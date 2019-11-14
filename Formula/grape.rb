42be867f913fb2002b6269621872f88b6f491282
Baptiste Fontaine <b@ptistefontaine.fr>
2019-11-15 00:03:29 +0100

    grape 0.2.0

--- Formula/grape.rb
+++ Formula/grape.rb
@@ -1,8 +1,8 @@
 class Grape < Formula
   desc "Syntax-aware grep-like for Clojure"
   homepage "https://github.com/bfontaine/grape"
-  url "https://github.com/bfontaine/grape/releases/download/0.1.0/grape-0.1.0-standalone.jar"
-  sha256 "47419e97f8f4a43af9117892bd4f73c1111a58896c9659242eb3f7a0731b97c1"
+  url "https://github.com/bfontaine/grape/releases/download/0.2.0/grape-0.2.0-standalone.jar"
+  sha256 "8245d12c00e92c5bd05e5a39aa92b9cc7881ec26c05abf1466aeb0270f5a0a30"
 
   bottle :unneeded
 
