14ca8416b7be017e697491ba38d59ee60770dd3f
Baptiste Fontaine <b@ptistefontaine.fr>
2020-03-28 15:09:15 +0100

    wpcleaner: add missing resources

--- Formula/wpcleaner.rb
+++ Formula/wpcleaner.rb
@@ -9,9 +9,12 @@ class Wpcleaner < Formula
       :using => :nounzip
 
     # from https://tools.wmflabs.org/wpcleaner/wpcleaner/digest.txt
-    %w[libs/commons-codec.jar
+    %w[getdown.txt
+       WPCleaner.jar
+       libs/commons-codec.jar
        libs/commons-compress.jar
        libs/commons-httpclient.jar
+       libs/commons-lang3.jar
        libs/commons-logging.jar
        libs/gettext-commons.jar
        libs/jackson-annotations.jar
@@ -19,13 +22,15 @@ class Wpcleaner < Formula
        libs/jackson-databind.jar
        libs/jaxen.jar
        libs/jdom.jar
-       libs/xercesImpl.jar
-       libs/xml-apis.jar
        libs/logback-classic.jar
        libs/logback-core.jar
        libs/slf4j-api.jar
+       libs/xercesImpl.jar
+       libs/xml-apis.jar
        logback.xml
        WPCleaner.png
+       commons-nuvola-web-broom-64px.png
+       commons-nuvola-web-broom.png
        WPCleaner.ico
        Bot.sh
        WPCleaner.sh
@@ -35,6 +40,10 @@ class Wpcleaner < Formula
        libs/NOTICE_commons-compress.txt
        libs/LICENSE_commons-httpclient.txt
        libs/NOTICE_commons-httpclient.txt
+       libs/LICENSE_commons-io.txt
+       libs/NOTICE_commons-io.txt
+       libs/LICENSE_commons-lang3.txt
+       libs/NOTICE_commons-lang3.txt
        libs/LICENSE_commons-logging.txt
        libs/NOTICE_commons-logging.txt
        libs/LICENSE_gettext-commons.txt
@@ -42,7 +51,17 @@ class Wpcleaner < Formula
        libs/LICENSE_jdom.txt
        libs/LICENSE_jaxen.txt
        libs/LICENSE_xerces.txt
-       libs/NOTICE_xerces.txt].each do |path|
+       libs/NOTICE_xerces.txt
+       libs/getdown-launcher-1.8.6.jar
+       tasks/cswiki/_Weekly.txt
+       tasks/enwiki/_Weekly.txt
+       tasks/enwiki/ISBN_ISSN.txt
+       tasks/enwiki/ListCheckWiki.txt
+       tasks/frwiki/_Daily.txt
+       tasks/frwiki/_Weekly.txt
+       tasks/frwiki/ISBN_ISSN.txt
+       tasks/frwiki/ListCheckWiki.txt
+       tasks/frwiki/UpdateCheckWiki.txt].each do |path|
         resource path do
           url "https://tools.wmflabs.org/wpcleaner/wpcleaner/#{path}"
         end
