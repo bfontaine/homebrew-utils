e7bbad2f2267cbe0185048d759727a3456532043
Baptiste Fontaine <b@ptistefontaine.fr>
2020-10-27 12:19:12 +0100

    grape 0.5.0

--- Formula/grape.rb
+++ Formula/grape.rb
@@ -1,14 +1,14 @@
 class Grape < Formula
   desc "Syntax-aware grep-like for Clojure"
   homepage "https://github.com/bfontaine/grape"
-  version "0.4.0"
+  version "0.5.0"
 
   if OS.mac?
-    url "https://github.com/bfontaine/grape/releases/download/0.4.0/grape-0.4.0-macos-amd64.zip"
-    sha256 "85c35cdc04a0eec2b72596f0dbe33e03a5139699af7319b58fe915c3dd121f28"
+    url "https://github.com/bfontaine/grape/releases/download/0.5.0/grape-macos-amd64.zip"
+    sha256 "fc869e1bd32bbf4c39c032657eaf4495da201a8882c705c87cfdd28367770a3c"
   else
-    url "https://github.com/bfontaine/grape/releases/download/0.4.0/grape-0.4.0-linux-amd64.zip"
-    sha256 "6fc6033e0abf2aced6737021bb3ffa7f69d6715a8bcdcf549b00325c9ab87c73"
+    url "https://github.com/bfontaine/grape/releases/download/0.5.0/grape-linux-amd64.zip"
+    sha256 "9b3fcb2dcc8b48278e3ce7252c5dc8858adc824a977989aba2cf16aa44efef62"
   end
 
   bottle :unneeded
@@ -25,7 +25,7 @@ class Grape < Formula
       [x]
       (* x 42))
     EOS
-    assert_equal "(* x 42)",
+    assert_equal "5:  (* x 42)",
       shell_output("#{bin}/grape '(* x $)' #{testpath}/test.clj").strip
   end
 end
