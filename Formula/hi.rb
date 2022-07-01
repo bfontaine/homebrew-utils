aa7c0b1437f5c6aefc45fd1d2ea91e148ebd339e
Baptiste Fontaine <baptiste@bixoto.com>
2022-07-01 10:44:34 +0200

    hi 0.7

--- Formula/hi.rb
+++ Formula/hi.rb
@@ -1,10 +1,10 @@
 class Hi < Formula
   desc "Host info for your shell"
   homepage "https://github.com/nikopol/hi"
-  url "https://raw.githubusercontent.com/nikopol/hi/142ae3026aab757f4f33dbc30023e9868ab41046/hi",
-    :nounzip => true
-  version "0.4"
-  sha256 "11f71440653880c8f1eb2ebfd95232823558264f4285f6d1c461ac3544c2c49d"
+  url "https://raw.githubusercontent.com/nikopol/hi/e875673b9bfc64459d4c5b4b746b9e8e7fc9ba3e/hi",
+    nounzip: true
+  version "0.7"
+  sha256 "10dc0e30a3d751732f5e29412d20e93658093a0a63376b607c7e4ccd419e0b7d"
 
   def install
     bin.install "hi"
