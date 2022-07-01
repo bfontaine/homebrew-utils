91833975b0f3471cc587cab4935b52937f72a90f
Baptiste Fontaine <baptiste@bixoto.com>
2022-07-01 10:57:00 +0200

    mozart2 2.0.1
    WIP: build is broken

--- Formula/mozart2.rb
+++ Formula/mozart2.rb
@@ -2,12 +2,14 @@ class Mozart2 < Formula
   desc "Mozart Programming System v2"
   homepage "https://github.com/mozart/mozart2"
   head "https://github.com/mozart/mozart2.git"
+  url "https://github.com/mozart/mozart2/archive/refs/tags/v2.0.1.tar.gz"
+  sha256 "0670e95f90068b87cce42b7e216e0cd2b162859eab10e9266290a3274275e2b6"
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "emacs"
-  depends_on "homebrew/versions/llvm33"
-  depends_on "homebrew/dupes/tcl-tk"
+  depends_on "tcl-tk"
+  depends_on "llvm"
   depends_on "openjdk"
 
   def install
@@ -18,7 +20,7 @@ class Mozart2 < Formula
     cmake_args << "-DCMAKE_BUILD_TYPE=Release"
 
     emacs_prefix = Formula["emacs"].opt_prefix
-    llvm_prefix = Formula["llvm_for_mozart2"].opt_prefix
+    llvm_prefix = Formula["llvm"].opt_prefix
 
     cmake_args << "-DEMACS=#{emacs_prefix}/bin/emacs"
 
@@ -28,17 +30,25 @@ class Mozart2 < Formula
     cmake_args << "-DLLVM_SRC_DIR=#{llvm_prefix}"
     cmake_args << "-DLLVM_BUILD_DIR=#{llvm_prefix}"
 
-    # Set flags to use libc++ and C++0x headers
-    cpp_headers_dir = %w[
-      /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
-      /Library/Developer/CommandLineTools/usr/lib/c++/v1
-      /usr/lib/c++/v1
-    ].find { |dir| File.exist?("#{dir}/forward_list") }
-    raise "Could not find C++0x headers on #{MacOS.version}" unless cpp_headers_dir
+    on_macos do
+      # Set flags to use libc++ and C++0x headers
+      cpp_headers_dir = %w[
+        /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
+        /Library/Developer/CommandLineTools/usr/lib/c++/v1
+        /usr/lib/c++/v1
+      ].find { |dir| File.exist?("#{dir}/forward_list") }
+      raise "Could not find C++0x headers" unless cpp_headers_dir
 
-    cmake_args << "-DCMAKE_CXX_FLAGS=-stdlib=libc++ -I#{cpp_headers_dir}"
+      cmake_args << "-DCMAKE_CXX_FLAGS=-stdlib=libc++ -I#{cpp_headers_dir}"
+    end
+
+    on_linux do
+      cmake_args << "-DCMAKE_CXX_COMPILER=#{llvm_prefix}/bin/clang++"
+      cmake_args << "-DCMAKE_C_COMPILER=#{llvm_prefix}/bin/clang"
+    end
 
     mkdir "build" do
+      # XXX
       system "cmake", "..", *cmake_args
       system "make", "install"
 
