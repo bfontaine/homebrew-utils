0abeddac8d3c913d2560b18ed4d928342fa852bc
Baptiste Fontaine <baptiste@bixoto.com>
2024-10-28 12:23:41 +0100

    rfc 1.0.1

--- Formula/rfc.rb
+++ Formula/rfc.rb
@@ -1,8 +1,8 @@
 class Rfc < Formula
   desc "Bash tool to read RFCs from the command-line"
   homepage "https://github.com/bfontaine/rfc#readme"
-  url "https://github.com/bfontaine/rfc/archive/v1.0.0.tar.gz"
-  sha256 "ceb3e968a8f4c1caf35b15196a4151faa3632f9c5bae277d89de77d352b93172"
+  url "https://github.com/bfontaine/rfc/archive/refs/tags/v1.0.1/v1.0.1.tar.gz"
+  sha256 "65ed320e57d1bc746dc9fc98af307d8789e72fe85cf58f0cc4cc3e8b54a84843"
 
   head "https://github.com/bfontaine/rfc.git"
 
