b1406f3fc957f5ea4e0248a67e29447fbb344c62
Baptiste Fontaine <baptiste@bixoto.com>
2025-05-23 11:50:24 +0200

    mtftar: fix audit

--- Formula/mtftar.rb
+++ Formula/mtftar.rb
@@ -1,9 +1,9 @@
 class Mtftar < Formula
   desc "Filter to translate MTF/BKF backup streams to the TAR format"
   homepage "https://github.com/geocar/mtftar"
-  url "https://github.com/geocar/mtftar/archive/525e7e72fc6d05da448d14de29ada48eb5c8ccc1.zip"
+  url "https://github.com/geocar/mtftar/archive/525e7e72fc6d05da448d14de29ada48eb5c8ccc1.tar.gz"
   version "2007-02-05.14-55"
-  sha256 "7010c7e61d1f1c0f8d3f531b59255b1b0dacb76f75984df60a6712e4c0471479"
+  sha256 "8359cde73f82bceab69c78d1f01fd6ea4ae04ad42fa9f23850cac76ed5208f86"
   license "GPL-2.0"
 
   def install
